5b4a786ae8bf99381bde9e78f6f281ab7246e17d
Greg Carr <greghcarr@gmail.com>
2025-12-03 23:45:57 -0500

    Day 3.

---
+++ day3.py
@@ -0,0 +1,50 @@
+# ----- part 1 -----
+# day3test.txt solution: 98 + 89 + 78 + 92 = 357
+
+FILE_PATH = 'inputs/day3.txt'
+
+with open(FILE_PATH) as f:
+    inputs = f.read().splitlines()
+
+joltages = []
+for seq in inputs:
+    first_digit = seq[0]
+    first_digit_index = 0
+    # check only to the second to last digit
+    for i in range(len(seq) - 1):
+        if seq[i] > first_digit:
+            first_digit = seq[i]
+            first_digit_index = i
+    second_digit = seq[first_digit_index + 1]
+    for j in range(first_digit_index + 1, len(seq)):
+        if seq[j] > second_digit:
+            second_digit = seq[j]
+    joltage = int(f'{first_digit}{second_digit}')
+    joltages.append(joltage)
+
+print(f'Sum of joltages: {sum(joltages)}')
+
+
+# ----- part 2 -----
+
+FILE_PATH = 'inputs/day3.txt'
+
+with open(FILE_PATH) as f:
+    inputs = f.read().splitlines()
+
+joltages = []
+for seq in inputs:
+    last_digit_index = -1
+    joltage_digits = []
+    for h in range(11, -1, -1):
+        digit = 0
+        for i in range(last_digit_index + 1, len(seq) - h):
+            if int(seq[i]) > digit:
+                digit = int(seq[i])
+                last_digit_index = i
+        joltage_digits.append(str(digit))
+    joltages.append(int(''.join(joltage_digits)))
+
+
+# print(f'Joltages: {joltages}')
+print(f'Sum of joltages: {sum(joltages)}')
